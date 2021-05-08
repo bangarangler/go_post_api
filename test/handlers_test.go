d21e3896918feb377849546f27ca7691a04f1e4e
Jon <bangarangler@gmail.com>
2021-05-08 16:46:09 -0400

    wip

--- test/handlers_test.go
+++ test/handlers_test.go
@@ -1,19 +1,40 @@
 package test
 
 import (
-	"log"
+	// "log"
 	"testing"
-
-	"github.com/bangarangler/go_post_api/app"
-	"github.com/bangarangler/go_post_api/database"
 )
 
-func ensureTableExists() {
-	if _, err := a.DB.Exec(createSchema); err != nil {
-		log.Fatal(err)
+func TestWorking(t *testing.T) {
+	input := "Testing"
+	expected := "Testing"
+
+	if input != expected {
+		t.Errorf("\n Word %s expected to be 'Testing', but is %s \n", input, input)
 	}
 }
 
+// func ensureTableExists() {
+// 	if _, err := a.DB.Exec(createSchema); err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
+
+// const createSchema = `
+// 	CREATE TABLE IF NOT EXISTS posts
+// 	(
+// 			id SERIAL PRIMARY KEY,
+// 			title TEXT,
+// 			content TEXT,
+// 			author TEXT
+// 	)
+// `
+//
+// var insertPostSchema = `
+// 	INSERT INTO posts(title, content, author) VALUES($1,$2,$3) RETURNING id
+// `
+
 func TestGetPostHandler(t *testing.T) {}
 
-func TestCreatePostHanndler(t *testing.T) {}
+//
+// func TestCreatePostHanndler(t *testing.T) {}
