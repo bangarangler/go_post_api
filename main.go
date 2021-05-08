e18e0e43ab721082cad60f32fa785b7e39c78d60
Jon <bangarangler@gmail.com>
2021-05-08 14:12:17 -0400

    added app package with handlers.go...
    created check func and fired up to test routes are working with curl

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/bangarangler/go_post_api"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/bangarangler/go_post_api/app"
+)
 
 func main() {
-	app.New()
+	app := app.New()
+
+	http.HandleFunc("/", app.Router.ServeHTTP)
+
+	err := http.ListenAndServe(":9000", nil)
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		log.Println(e)
+		os.Exit(1)
+	}
 }
